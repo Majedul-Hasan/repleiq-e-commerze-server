cde5a1b49d1bd72703e2186827ba13b748df1038
Majedul Hasan <hasanmajedul@gmail.com>
2023-07-09 12:49:25 +0600

    user releted api

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { dbConnect, client } = require('./config/configDB');
+const { tokenPost, verifyJWT } = require('./middleware/authMiddleware');
 
 const morgan = require('morgan');
+const { ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5171;
@@ -11,6 +14,131 @@ app.use(cors());
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+// jwt
+app.post('/jwt', tokenPost);
+
+async function run() {
+  try {
+    dbConnect();
+    const database = client.db(process.env.DB_NAME);
+    const usersCollection = database.collection('users');
+    const productsCollection = database.collection('products');
+    const cartCollection = database.collection('carts');
+    const paymentCollection = database.collection('payments');
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res
+          .status(403)
+          .send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    };
+
+    // users
+    // create user
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const query = {};
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // users related apis
+    app.get('/users/subscriber', verifyJWT, verifyAdmin, async (req, res) => {
+      const query = { role: 'subscriber', _id: new ObjectId(id) };
+      const user = await usersCollection.findOne(query);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.send(user);
+    });
+    // users related apis
+    app.get(
+      '/users/subscriber/:id',
+      verifyJWT,
+      verifyAdmin,
+      async (req, res) => {
+        const query = { role: 'subscriber' };
+        const result = await usersCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
+
+    // user make admin
+    app.patch('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete(
+      '/users/admin/:user_id',
+      verifyJWT,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.user_id;
+
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+
+    app.post('/products', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.decoded.email;
+      const course = req.body;
+      const user = await usersCollection.findOne({ email: email });
+      console.log(user);
+
+      if (user) {
+        const result = await coursesCollection.insertOne(course);
+        console.log(result);
+        if (!user.courses) {
+          user.courses = []; // Initialize 'courses' as an empty array if it doesn't exist
+        }
+        user.courses.push(result.insertedId);
+        await usersCollection.updateOne(
+          { _id: new ObjectId(user._id) },
+          { $set: user }
+        );
+        console.log(result);
+        console.log(user);
+        res.send(result);
+      }
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/file.html');
